014173013f57d1f4d6a736723d23e47929139021
Threshold247 <65241733+Threshold247@users.noreply.github.com>
2022-12-06 19:03:14 +0200

    Changed Host name in server file

--- server.js
+++ server.js
@@ -4,21 +4,21 @@ const port = 3001;
 
 const { Pool } = require("pg");
 
-const connectionString =
-  "postgres://cyf_hotel_user:ZSJsNLO8dv7A2R6mtgmdkGwXZqINIqDY@dpg-ce5gfckgqg49410a5dn0-a.oregon-postgres.render.com/cyf_hotel";
-pool = new Pool({
-  connectionString,
-  ssl: {
-    rejectUnauthorized: true,
-  },
-});
-// const pool = new Pool({
-//   user: "cyf_hotel_user",
-//   host: "https://cyf_hotel_user:ZSJsNLO8dv7A2R6mtgmdkGwXZqINIqDY@dpg-ce5gfckgqg49410a5dn0-a/cyf_hotel",
-//   database: "cyf_hotels",
-//   password: "ZSJsNLO8dv7A2R6mtgmdkGwXZqINIqDY",
-//   port: 5432,
+// const connectionString =
+//   "postgres://cyf_hotel_user:ZSJsNLO8dv7A2R6mtgmdkGwXZqINIqDY@dpg-ce5gfckgqg49410a5dn0-a.oregon-postgres.render.com/cyf_hotel";
+// pool = new Pool({
+//   connectionString,
+//   ssl: {
+//     rejectUnauthorized: true,
+//   },
 // });
+const pool = new Pool({
+  user: "cyf_hotel_user",
+  host: "postgres://cyf_hotel_user:ZSJsNLO8dv7A2R6mtgmdkGwXZqINIqDY@dpg-ce5gfckgqg49410a5dn0-a/cyf_hotel",
+  database: "cyf_hotels",
+  password: "ZSJsNLO8dv7A2R6mtgmdkGwXZqINIqDY",
+  port: 5432,
+});
 
 app.get("/hotels", function (req, res) {
   console.log(req.method, req.url);
